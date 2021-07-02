3f5015605c6051324f7a97a9cd7432a3cfcc7a79
Nikolaos Ftylitakis <ftylitak@gmail.com>
2021-07-02 17:45:57 +0300

    initial implementation of Generic modem & Quectel MC60 support. Supports utility functions for controlling the modem, PPP setup and GPS retrieval.

---
+++ insighioNode/lib/networking/modem/modem.py
@@ -0,0 +1,185 @@
+import utime
+from machine import Pin, UART
+import ure
+import network
+
+
+class Modem:
+    def __init__(self, uart):
+        self.uart = uart
+        self.connected = False
+        self.ppp = None
+
+    def is_alive(self):
+        (status, response) = self.send_at_cmd("AT", 5000)
+        return status
+
+    def print_status(self):
+        self.send_at_cmd("ATI")
+        self.send_at_cmd("AT+CFUN?")
+        self.send_at_cmd("AT+CMEE=2")
+        self.send_at_cmd("AT+CPIN?")
+        self.send_at_cmd("AT+QDSIM?")
+        self.send_at_cmd("AT+QSIMVOL?")
+        self.send_at_cmd("AT+QSIMDET?")
+
+    def power_on(self):
+        # network registration
+        p0 = Pin(26, Pin.OUT)
+        p0.on()
+        # modem boot
+        pinNum = 23
+        p0 = Pin(pinNum, Pin.OUT)
+        p0.on()
+        print("Output Pin {} {}".format(pinNum, p0.value()))
+        utime.sleep_ms(1200)
+        p0.off()
+        print("Output Pin {} {}".format(pinNum, p0.value()))
+        utime.sleep_ms(2000)
+
+    def power_off(self):
+        pinNum = 23
+        p0 = Pin(pinNum, Pin.OUT)
+        p0.on()
+        print("Output Pin {} {}".format(pinNum, p0.value()))
+        utime.sleep_ms(800)
+        p0.off()
+
+    def init(self):
+        if not self.is_alive():
+            self.power_on()
+
+        if self.is_alive():
+            self.send_at_cmd('ATE0')  # disable command exho
+            return True
+
+        # set auto-registration
+        self.send_at_cmd("AT+COPS=0")
+
+        # disable unsolicited report of network registration
+        self.send_at_cmf("AT+CREG=0")
+
+        return False
+
+    def wait_for_registration(self, timeoutms=30000):
+        status = False
+
+        start_timestamp = utime.ticks_ms()
+        timeout_timestamp = start_timestamp + timeoutms
+        regex_creg = "\\+CREG:\\s+\\d,(\\d)"
+        while utime.ticks_ms() < timeout_timestamp:
+            (status, lines) = self.send_at_cmd('AT+CREG?')
+            if status and len(lines) > 0:
+                regex_match = None
+                for line in lines:
+                    regex_match = ure.search(regex_creg, line)
+                    if regex_match and regex_match.group(1) == "1":
+                        return True
+            utime.sleep_ms(1000)
+        return False
+
+    def connect(self, timeoutms=30000):
+        (status, lines) = self.send_at_cmd('AT+CGDATA="PPP",1', 30000, "CONNECT")
+        if not status:
+            return False
+
+        self.ppp = network.PPP(self.uart)
+        self.ppp.active(True)
+        self.ppp.connect()
+
+        start_timestamp = utime.ticks_ms()
+        timeout_timestamp = start_timestamp + timeoutms
+        while utime.ticks_ms() < timeout_timestamp:
+            self.connected = self.is_connected()
+            if self.connected:
+                break
+            utime.sleep_ms(100)
+
+        return self.connected
+
+    def is_connected(self):
+        if self.ppp:
+            return self.ppp.isconnected()
+        else:
+            return False
+
+    def disconnect(self):
+        if self.ppp:
+            self.ppp.active(False)
+
+        self.connected = False
+        (status, _) = self.send_at_cmd("AT+CGACT=0,1")
+        return status
+
+    # to be overriden by children
+    def set_gps_state(self, poweron=True):
+        pass
+
+    # to be overriden by children
+    def is_gps_on(self):
+        return False
+
+    # to be overriden by children
+    def get_gps_position(self, timeoutms=300000):
+        pass
+
+    def send_at_cmd(self, command, timeoutms=30000, success_condition="OK"):
+        status = None
+        responseLines = []
+
+        if self.connected:
+            responseLines = ['error: ppp connection active']
+            return (status, responseLines)
+
+        # clear incoming message buffer
+        while self.uart.any():
+            print(self.uart.readline())
+
+        print("> " + command)
+        write_success = self.uart.write(command + '\r\n')
+        if not write_success:
+            return (status, ['error: can not send command'])
+
+        start_timestamp = utime.ticks_ms()
+        timeout_timestamp = start_timestamp + timeoutms
+
+        success_regex = "^(\\w+\\s+)?(" + success_condition + ")$"
+        error_regex = "^((\\w+\\s+)?(ERROR|FAIL)$)|(\\+CM[ES] ERROR)"
+        first_line = True
+
+        while True:
+            remaining_bytes = self.uart.any()
+            if(utime.ticks_ms() >= timeout_timestamp):
+                status = False
+                break
+
+            # if OK or ERROR has already been read though there are still
+            # data on the UART, keep reading
+            if status is not None and remaining_bytes == 0:
+                break
+
+            line = self.uart.readline()
+            try:
+                line = line if line is None else line.decode('utf8').strip()
+            except Exception as e:
+                print("! " + str(line))
+                line = ""
+
+            if line:
+                if first_line:
+                    print("< " + str(line))
+                    first_line = False
+                else:
+                    print("  " + str(line))
+                responseLines.append(line)
+                if ure.search(success_regex, line) is not None:
+                    status = True
+                elif ure.search(error_regex, line) is not None:
+                    status = False
+
+            utime.sleep_ms(50)
+
+        if status is None:
+            status = False
+
+        return (status, responseLines)
---
+++ insighioNode/lib/networking/modem/modem_mc60.py
@@ -0,0 +1,47 @@
+from modem import Modem
+import utime
+import ure
+from micropyGPS.micropyGPS import MicropyGPS
+
+
+class ModemMC60(Modem):
+    def __init__(self, uart):
+        super().__init__(uart)
+
+    def set_gps_state(self, poweron=True):
+        self.send_at_cmd('AT+QGNSSC=' + "1" if poweron else "0")
+
+    # to be overriden by children
+    def is_gps_on(self):
+        (status, lines) = self.send_at_cmd('AT+QGNSSC?')
+        reg = "\\+QGNSSC:\\s+(\\d)"
+        if status and len(lines):
+            res = ure.match(reg, lines[0])
+            return res is not None and res.group(1) == "1"
+        return False
+
+    def get_gps_position(self, timeoutms=300000, satelite_number_threshold=6):
+        counter = 0
+        gps_fix = False
+        print("Starting query gps")
+        my_gps = MicropyGPS()
+
+        start_timestamp = utime.ticks_ms()
+        timeout_timestamp = start_timestamp + timeoutms
+        while utime.ticks_ms() < timeout_timestamp:
+
+            counter += 1
+            (status, lines) = self.send_at_cmd('AT+QGNSSRD="NMEA/GGA"')
+            if status and len(lines) > 0:
+                if lines[0].startswith('+QGNSSRD:'):
+                    lines[0] = lines[0].replace('+QGNSSRD:', '')
+                for line in lines:
+                    for char in line:
+                        my_gps.update(char)
+                    print("{} Lat: {}, Lon: {}, NumSats: {}".format(my_gps.timestamp, my_gps.latitude, my_gps.longitude, my_gps.satellites_in_use))
+                    if my_gps.satellites_in_use >= satelite_number_threshold:
+                        gps_fix = True
+                        return (my_gps.timestamp, my_gps.latitude, my_gps.longitude, my_gps.satellites_in_use)
+            utime.sleep_ms(1000)
+
+        return (None, None, None, None)
