a1a8d01b85b4d35688b86405ecc797262a3c3bb5
Nikolaos Ftylitakis <ftylitak@gmail.com>
2021-07-15 10:43:40 +0300

    after adopting string value for band configuration, cast string to int when band used in cellular

--- insighioNode/lib/networking/cellular.py
+++ insighioNode/lib/networking/cellular.py
@@ -129,7 +129,7 @@ def connect(cfg, dataStateOn=True):
 
             # start attachment
             logging.debug('Attaching...')
-            # lte.attach(band=cfg._BAND, apn=cfg._APN, legacyattach=False)
+            # lte.attach(band=int(cfg._BAND), apn=cfg._APN, legacyattach=False)
             while not modemInst.is_attached() and (utime.ticks_ms() < attachment_timeout):
                 utime.sleep_ms(10)
 
