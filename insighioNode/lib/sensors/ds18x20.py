b3e3e17f77b882766fab96bb4b1965c54b12d9ed
Nikolaos Ftylitakis <ftylitak@gmail.com>
2021-09-10 16:25:53 +0300

    @ds18x20, if board is ESP32, use internal modules The procedure on how to read the temperature is based on the board type and loaded modules.

--- insighioNode/lib/sensors/ds18x20.py
+++ insighioNode/lib/sensors/ds18x20.py
@@ -9,27 +9,39 @@
 import utime
 import sensors
 import machine
-try:
-    from onewire import OneWire
-except:
-    from sensors.protocol.onewire import OneWire
+import sys
 
-try:
+if sys.platform == 'esp32':
+    from onewire import OneWire
     from ds18x20 import DS18X20
-except:
-    from sensor.ds18x20 import DS18X20
+else:
+    from sensors.protocol.onewire import OneWire
+    from sensors.sensor.ds18x20 import DS18X20
+
 
 def get_reading(data_pin, vcc_pin=None):
     """ Returns temperature reading, for given VCC and DATA pins """
     sensors.set_sensor_power_on(vcc_pin)
 
-    # initialize DS18X20
-    ow = OneWire(machine.Pin(data_pin))
-    tempObj = DS18X20(ow)
-    # sensor reading
-    tempObj.start_conversion()
-    utime.sleep(1)
-    temp = tempObj.read_temp_async()
+    try:
+        # initialize DS18X20
+        ow = OneWire(machine.Pin(data_pin))
+        tempObj = DS18X20(ow)
+
+        if sys.platform == 'esp32':
+            roms = [rom for rom in ow.scan() if rom[0] == 0x10 or rom[0] == 0x28]
+            tempObj.convert_temp()
+            utime.sleep_ms(750)
+            temp = tempObj.read_temp(roms[0])
+        else:
+            # sensor reading
+            tempObj.start_conversion()
+            utime.sleep(1)
+            temp = tempObj.read_temp_async()
+    except Exception as e:
+        import logging
+        logging.exception(e, "unable to read dx18x20 pin: " + str(data_pin))
+        temp = None
 
     sensors.set_sensor_power_off(vcc_pin)
 
