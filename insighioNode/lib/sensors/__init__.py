2a7d618831a763dea23e778a7c61accedcf093fb
Nikolaos Ftylitakis <ftylitak@gmail.com>
2021-09-10 16:27:31 +0300

    remove unnecessary sleep commands when opening closing power to the sensors.

--- insighioNode/lib/sensors/__init__.py
+++ insighioNode/lib/sensors/__init__.py
@@ -6,11 +6,10 @@ def set_sensor_power_on(vcc_pin):
     if vcc_pin is not None:
         # enable supply to sensor
         gpio_handler.set_pin_value(vcc_pin, 1)
-        utime.sleep_ms(500)
+        utime.sleep_ms(100)
 
 
 def set_sensor_power_off(vcc_pin):
     if vcc_pin is not None:
-        utime.sleep_ms(500)
         # disanable supply to sensor
         gpio_handler.set_pin_value(vcc_pin, 0)
