e037fc54dd6fcfca3d61edf8e368676cd4c2f031
Nikolaos Ftylitakis <ftylitak@gmail.com>
2021-12-27 12:32:57 +0200

    reverting changes of last commit (a5a58e6c4a4b8e64dd8b8754027ce445f4b09f88)

--- insighioNode/apps/demo_console/templ/cellular_config_templ.py
+++ insighioNode/apps/demo_console/templ/cellular_config_templ.py
@@ -3,8 +3,8 @@ network="cellular"
 """ System-related configuration options """
 _DEEP_SLEEP_PERIOD_SEC = <period>  # tx period in secs
 _BATCH_UPLOAD_MESSAGE_BUFFER = <batch-upload-buffer-size>
-_SCHEDULED_TIMESTAMP_A_SECOND = <scheduled_time_a>
-_SCHEDULED_TIMESTAMP_B_SECOND = <scheduled_time_b>
+_SCHEDULED_TIMESTAMP_A_SECOND = <scheduled-time-a>
+_SCHEDULED_TIMESTAMP_B_SECOND = <scheduled-time-b>
 
 """ Cellular-related configuration options """
 _MAX_ATTACHMENT_ATTEMPT_TIME_SEC = 90
