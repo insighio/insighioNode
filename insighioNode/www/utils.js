4ff01c8d1f94ece43eb8abb17b7348b0e603ac7f
Nikolaos Ftylitakis <ftylitak@gmail.com>
2022-05-08 12:03:24 +0300

    refactoring on all webui pages

--- insighioNode/www/utils.js
+++ insighioNode/www/utils.js
@@ -41,8 +41,40 @@ function showElement(elementId, status) {
     elem.style.display = status ? "block" : "none"
 }
 
-function setElementValueIfNotUndefined(elementId, newValue, defaultValue=""){
+function setElemValue(elementId, newValue, defaultValue=""){
   var elem = document.getElementById(elementId)
-  if(elem)
-    elem.value = newValue !== "undefined" && newValue !== undefined ? newValue : defaultValue
+  if(!elem) {
+    console.log("Element not found: ", elementId)
+    return
+  }
+
+  elem.value = (newValue !== "undefined" && newValue !== undefined) ? newValue : defaultValue
+}
+
+function validateElemValue(elemId, message, regex=undefined) {
+  var fieldObj = document.getElementById(elemId)
+  if(!fieldObj) {
+    console.log("Element not found: ", elemId)
+    return false
+  }
+
+  var value = fieldObj.value.trim()
+  if(value == "" || (regex && !((new RegExp(regex, 'g')).exec(value)))) {
+      fieldObj.style.borderColor = "red";
+      fieldObj.focus()
+      window.alert("Please enter a valid " + message)
+      return false
+  }
+  fieldObj.style.borderColor = "green";
+  return true
+}
+
+function boolElemToPyStr(elemId, boolField="checked") {
+  var fieldObj = document.getElementById(elemId)
+  if(!fieldObj) {
+    console.log("Element not found: ", elemId)
+    return false
+  }
+
+  return fieldObj[boolField] ? "True" : "False"
 }
