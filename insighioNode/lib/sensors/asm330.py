a4774c07001dd85bcab109af254f3da5ee2a3e70
Nikolaos Ftylitakis <ftylitak@gmail.com>
2022-10-11 17:51:58 +0300

    make asm330 object global to rapidly access accelerometer data

--- insighioNode/lib/sensors/asm330.py
+++ insighioNode/lib/sensors/asm330.py
@@ -3,6 +3,7 @@ import sensors
 import logging
 from machine import Pin, SoftI2C
 import ubinascii
+import gc
 
 asm330_addr = 0x6A
 _CTRL1_XL = 0x10 # Accelerometer control register
@@ -15,6 +16,8 @@ _OUTZ_H_A = 0x2D # Linear acceleration sensor Z-axis output register HIGH
 _OUTZ_L_A = 0x2C # Linear acceleration sensor Z-axis output register LOW
 _STANDARD_GRAVITY = 9.80665
 
+i2c_obj=None
+
 
 def _to_int(val, nbits):
     i = int(val, 16)
@@ -25,10 +28,12 @@ def _to_int(val, nbits):
 def get_sensor_whoami():
     """ Returns reading """
     #sensors.set_sensor_power_on(vcc_pin)
-    i2c=SoftI2C(scl=Pin(38),sda=Pin(39))
+    if not i2c_obj:
+        logging.debug("XL not initialized")
+        return
     try:
         # WHO AM I -->  this should return 6B
-        val = i2c.readfrom_mem(asm330_addr, 0x0F, 1)
+        val = i2c_obj.readfrom_mem(asm330_addr, 0x0F, 1)
         logging.debug("Data {}, Output: {}".format(val, ubinascii.hexlify(val)))
     except Exception as e:
         logging.exception(e, "Exception raised in I2C {}")
@@ -37,20 +42,30 @@ def get_sensor_whoami():
 
     return None
 
-def set_asm(odr_xl=0, fs_xl=0, lpf_xl_en=0):
+def init(scl_pin, sda_pin):
+    global i2c_obj
+    i2c_obj=SoftI2C(scl=Pin(scl_pin),sda=Pin(sda_pin))
+    _set_asm()
+
+def _set_asm(odr_xl=0, fs_xl=0, lpf_xl_en=0):
     """ Accelerometer ODR selection (CTRL1_XL)"""
-    i2c=SoftI2C(scl=Pin(38),sda=Pin(39))
+    if not i2c_obj:
+        logging.debug("XL not initialized")
+        return
+
     if odr_xl > 10:
         logging.debug("Invalid XL ODR Setting (acceptable values: 0 .. 10)")
     else:
         try:
-            i2c.writeto_mem(asm330_addr, _CTRL1_XL, b'\x10')
+            i2c_obj.writeto_mem(asm330_addr, _CTRL1_XL, b'\x10')
         except Exception as e:
             logging.exception(e, "Exception raised in I2C {}")
 
 def get_acc_reading():
     """ Returns acc reading """
-    i2c=SoftI2C(scl=Pin(38),sda=Pin(39))
+    if not i2c_obj:
+        logging.debug("XL not initialized")
+        return
     #sensors.set_sensor_power_on(vcc_pin)
     ACC_X = None
     ACC_Y = None
@@ -58,9 +73,9 @@ def get_acc_reading():
     try:
         #while(True):
         # LINEAR ACCELERATION X,Y,Z
-        ACC_X = _to_int(ubinascii.hexlify(i2c.readfrom_mem(asm330_addr, _OUTX_H_A, 1)+i2c.readfrom_mem(asm330_addr, _OUTX_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
-        ACC_Y = _to_int(ubinascii.hexlify(i2c.readfrom_mem(asm330_addr, _OUTY_H_A, 1)+i2c.readfrom_mem(asm330_addr, _OUTY_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
-        ACC_Z = _to_int(ubinascii.hexlify(i2c.readfrom_mem(asm330_addr, _OUTZ_H_A, 1)+i2c.readfrom_mem(asm330_addr, _OUTZ_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
+        ACC_X = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTX_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTX_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
+        ACC_Y = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTY_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTY_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
+        ACC_Z = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTZ_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTZ_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
         logging.debug("[ACC-X] {} | [ACC-Y] {} | [ACC-Z] {}".format(ACC_X,ACC_Y,ACC_Z))
         #utime.sleep_ms(1000)
     except Exception as e:
