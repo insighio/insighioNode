2788b79e7606c5b47ef56039af4ddcae35925893
Nikolaos Ftylitakis <ftylitak@gmail.com>
2022-12-09 11:21:03 +0200

    optimizing accelerometer reading

--- insighioNode/lib/sensors/asm330.py
+++ insighioNode/lib/sensors/asm330.py
@@ -4,30 +4,25 @@ import logging
 from machine import Pin, SoftI2C
 import ubinascii
 import gc
+import ustruct
 
 asm330_addr = 0x6A
 _CTRL1_XL = 0x10 # Accelerometer control register
 _CTRL2_G  = 0x11 # Gyroscope control register
-_OUTX_H_A = 0x29 # Linear acceleration sensor X-axis output register HIGH
-_OUTX_L_A = 0x28 # Linear acceleration sensor X-axis output register LOW
-_OUTY_H_A = 0x2B # Linear acceleration sensor Y-axis output register HIGH
-_OUTY_L_A = 0x2A # Linear acceleration sensor Y-axis output register LOW
 _OUTZ_H_A = 0x2D # Linear acceleration sensor Z-axis output register HIGH
 _OUTZ_L_A = 0x2C # Linear acceleration sensor Z-axis output register LOW
+_OUTY_H_A = 0x2B # Linear acceleration sensor Y-axis output register HIGH
+_OUTY_L_A = 0x2A # Linear acceleration sensor Y-axis output register LOW
+_OUTX_H_A = 0x29 # Linear acceleration sensor X-axis output register HIGH
+_OUTX_L_A = 0x28 # Linear acceleration sensor X-axis output register LOW
 _STANDARD_GRAVITY = 9.80665
+
+_FACTOR = (2.0/32768)*_STANDARD_GRAVITY
 
 i2c_obj=None
 
-
-def _to_int(val, nbits):
-    i = int(val, 16)
-    if i >= 2 ** (nbits - 1):
-        i -= 2 ** nbits
-    return i
-
 def get_sensor_whoami():
     """ Returns reading """
-    #sensors.set_sensor_power_on(vcc_pin)
     if not i2c_obj:
         logging.debug("XL not initialized")
         return
@@ -37,8 +32,6 @@ def get_sensor_whoami():
         logging.debug("Data {}, Output: {}".format(val, ubinascii.hexlify(val)))
     except Exception as e:
         logging.exception(e, "Exception raised in I2C {}")
-
-    #sensors.set_sensor_power_off(vcc_pin)
 
     return None
 
@@ -61,24 +54,21 @@ def _set_asm(odr_xl=0, fs_xl=0, lpf_xl_en=0):
         except Exception as e:
             logging.exception(e, "Exception raised in I2C {}")
 
-def get_acc_reading():
+def get_reading():
     """ Returns acc reading """
     if not i2c_obj:
         logging.debug("XL not initialized")
         return
-    #sensors.set_sensor_power_on(vcc_pin)
+
     ACC_X = None
     ACC_Y = None
     ACC_Z = None
     try:
-        #while(True):
         # LINEAR ACCELERATION X,Y,Z
-        ACC_X = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTX_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTX_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
-        ACC_Y = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTY_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTY_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
-        ACC_Z = _to_int(ubinascii.hexlify(i2c_obj.readfrom_mem(asm330_addr, _OUTZ_H_A, 1) + i2c_obj.readfrom_mem(asm330_addr, _OUTZ_L_A, 1)), 16)*(2.0/32768)*_STANDARD_GRAVITY
-        logging.debug("[ACC-X] {} | [ACC-Y] {} | [ACC-Z] {}".format(ACC_X,ACC_Y,ACC_Z))
-        #utime.sleep_ms(1000)
+        data = i2c_obj.readfrom_mem(asm330_addr, _OUTX_L_A, 6)
+        ACC_X = ustruct.unpack_from("<h", data, 0)[0] * _FACTOR
+    	ACC_Y = ustruct.unpack_from("<h", data, 2)[0] * _FACTOR
+    	ACC_Z = ustruct.unpack_from("<h", data, 4)[0] * _FACTOR
     except Exception as e:
         logging.exception(e, "Exception raised in I2C {}")
-    #sensors.set_sensor_power_off(vcc_pin)
     return (ACC_X, ACC_Y, ACC_Z)
