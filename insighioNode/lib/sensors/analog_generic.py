ff607e22db9284ff0fa5d6e6a74cd522df9ae80a
Nikolaos Ftylitakis <ftylitak@gmail.com>
2023-02-21 13:07:35 +0200

    add exception handling in case of wrong Pin in analog sensor

--- insighioNode/lib/sensors/analog_generic.py
+++ insighioNode/lib/sensors/analog_generic.py
@@ -13,8 +13,12 @@ def get_reading(data_pin, voltage_divider=1, vcc_pin=None):
     sensors.set_sensor_power_on(vcc_pin)
 
     # get measurement
-    vmeas = gpio_handler.get_input_voltage(data_pin, voltage_divider)
-    logging.debug("Pin: [" + str(data_pin) + "] voltage reading : {} (mV)".format(vmeas))
+    vmeas = None
+    try:
+        vmeas = gpio_handler.get_input_voltage(data_pin, voltage_divider)
+        logging.debug("Pin: [{}] voltage reading : {} (mV)".format(str(data_pin), vmeas))
+    except Exception as e:
+        logging.exception(e, "Pin: [{}]".format(str(data_pin)))
     # here we need to add some calibration processing
 
     sensors.set_sensor_power_off(vcc_pin)
