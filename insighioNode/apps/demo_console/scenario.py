6f0c513b933f33c1f610ea89e739a40bb2e1bead
Nikolaos Ftylitakis <ftylitak@gmail.com>
2023-05-16 13:17:47 +0300

    add platform info in try/catch as micropython 1.16 did not support it

--- insighioNode/apps/demo_console/scenario.py
+++ insighioNode/apps/demo_console/scenario.py
@@ -279,8 +279,12 @@ def executeDeviceStatisticsUpload(cfg, network):
     stats["fw_v_commit"] = commit
     stats["free_flash"] = device_info.get_free_flash()
     stats["serial"] = device_info.get_device_id()[0]
-    import platform
-    stats["platform"]=platform.platform()
+    try:
+        import platform
+        stats["platform"]=platform.platform()
+    except:
+        logging.info("Skipping platform info.")
+
     logging.info("Uploading device statistics.")
     network.send_control_message(cfg, network.create_message(None, stats), "/stat")
 
