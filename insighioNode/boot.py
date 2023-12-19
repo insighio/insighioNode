254d8de3b7dc59f33c3e61c8a026aaa8fbf6585e
Nikolaos Ftylitakis <ftylitak@gmail.com>
2023-12-19 13:12:44 +0200

    set minimum voltage to 3.3

--- insighioNode/boot.py
+++ insighioNode/boot.py
@@ -2,7 +2,7 @@
 
 # if on battery, enable this for early low voltage detection.
 import gpio_handler
-
+#
 print("---Checking Voltage")
-gpio_handler.check_minimum_voltage_threshold()
+gpio_handler.check_minimum_voltage_threshold(3300)
 print("---Voltage OK")
