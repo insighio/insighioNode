44d2e69fe70f88606931c829dc104110c6423015
Nikolaos Ftylitakis <ftylitak@gmail.com>
2024-03-08 12:03:51 +0200

    optimization of ULP for better operation

--- insighioNode/apps/demo_console/scenario_pcnt_ulp.py
+++ insighioNode/apps/demo_console/scenario_pcnt_ulp.py
@@ -3,11 +3,11 @@ import machine
 import utils
 import utime
 import logging
-from external.kpn_senml.senml_unit import SenmlUnits
-from .dictionary_utils import set_value, set_value_int
-from . import cfg
+from external.kpn_senml.senml_unit import SenmlUnits, SenmlSecondaryUnits
+from .dictionary_utils import set_value, set_value_int, set_value_float
 
 _is_first_run = True
+_is_after_initialization = False
 
 def setup_assembly(is_high_freq, port_number):
     _edge_not_detected_script = "halt" if not is_high_freq else "SLEEP 100\njump entry"
@@ -36,8 +36,14 @@ loop_detected:
     return """\
 
 #define DR_REG_RTCIO_BASE            0x60008400
+#define RTC_IO_TOUCH_PAD2_REG        (DR_REG_RTCIO_BASE + 0x8c)
+#define RTC_IO_TOUCH_PAD2_MUX_SEL_M  (BIT(19))
+#define RTC_IO_TOUCH_PAD2_FUN_IE_M   (BIT(13))
 #define RTC_GPIO_IN_REG              (DR_REG_RTCIO_BASE + 0x24)
 #define RTC_GPIO_IN_NEXT_S           10
+#define DR_REG_SENS_BASE             0x60008800
+#define SENS_SAR_PERI_CLK_GATE_CONF_REG  (DR_REG_SENS_BASE + 0x104)
+#define SENS_IOMUX_CLK_EN            (BIT(31))
 
   /* Define variables, which go into .bss section (zero-initialized data) */
 
@@ -56,8 +62,18 @@ entry:
 	move r3, io_number
 	ld r3, r3, 0
 
+    # enable IOMUX clock
+    WRITE_RTC_FIELD(SENS_SAR_PERI_CLK_GATE_CONF_REG, SENS_IOMUX_CLK_EN, 1)
+
+    # connect GPIO to the RTC subsystem so the ULP can read it
+    WRITE_RTC_REG(RTC_IO_TOUCH_PAD2_REG, RTC_IO_TOUCH_PAD2_MUX_SEL_M, 1, 1)
+
+    # switch the GPIO into input mode
+    WRITE_RTC_REG(RTC_IO_TOUCH_PAD2_REG, RTC_IO_TOUCH_PAD2_FUN_IE_M, 1, 1)
+
 	/* Read the value of lower 16 RTC IOs into R0 */
 	READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S, 16)
+
 	rsh r0, r0, r3
 	and r0, r0, 1
 	/* State of input changed? */
@@ -140,16 +156,20 @@ def setval(start=0, value=0x0):
     machine.mem32[ULP_MEM_BASE + start * 4] = value
 
 
-def read_ulp_values(measurements, formula):
+def read_ulp_values(measurements, multiplier, label=None):
     logging.info("Reading pulse counters from ULP...")
 
     # read registers
-    edge_cnt_16bit = value(1)
-    loops = value(2)
+    if _is_after_initialization:
+        edge_cnt_16bit = 0
+        loops = 0
+    else:
+        edge_cnt_16bit = value(1)
+        loops = value(2)
 
     # read last pcnt saved timestamp
     last_timestamp = utils.readFromFlagFile(TIMESTAMP_FLAG_FILE)
-    now_timestamp = utime.time()
+    now_timestamp = utime.time_ns()
 
     logging.debug("last_timestamp: {}, now_timestamp: {}".format(last_timestamp, now_timestamp))
 
@@ -165,7 +185,8 @@ def read_ulp_values(measurements, formula):
     try:
         logging.debug("ULP timing: now_timestamp: {}, last_timestamp: {}".format(now_timestamp, last_timestamp))
         time_diff_from_prev = now_timestamp - int(last_timestamp)
-        if time_diff_from_prev < 0:
+        time_diff_from_prev = time_diff_from_prev / 1E9
+        if time_diff_from_prev <= 0:
             time_diff_from_prev = 0
         elif time_diff_from_prev > 86400:  # if bigger than one day
             # check if epoch_diff is stored
@@ -183,34 +204,34 @@ def read_ulp_values(measurements, formula):
     except:
         pass
 
+
     edge_cnt = 65536 * loops + edge_cnt_16bit
+    pulse_cnt = edge_cnt / 2
 
-    set_value_int(measurements, "pcnt_count", edge_cnt / 2, SenmlUnits.SENML_UNIT_COUNTER)
+    set_value_float(measurements, "pcnt_count", pulse_cnt, SenmlUnits.SENML_UNIT_COUNTER)
     set_value_int(measurements, "pcnt_edge_count", edge_cnt, SenmlUnits.SENML_UNIT_COUNTER)
-    set_value_int(measurements, "pcnt_period_s", time_diff_from_prev, SenmlUnits.SENML_UNIT_SECOND)
+    set_value_float(measurements, "pcnt_period_s", time_diff_from_prev, SenmlUnits.SENML_UNIT_SECOND)
 
-    if formula and formula != "v":
-        try:
-            raw_value = edge_cnt / 2
-            formula = formula.replace("v", str(raw_value))
-            formula = formula.replace("t", str(time_diff_from_prev))
-            to_execute = "v_transformed=({})".format(formula)
-            namespace = {}
-            exec(to_execute, namespace)
-            set_value(measurements, "pcnt_count_formula", namespace["v_transformed"])
-        except Exception as e:
-            logging.exception(e, "transformator name:{}, raw_value:{}, code:{}".format(name, raw_value, transformator))
+    pcnt_multiplier = 1
+    try:
+        pcnt_multiplier = int(multiplier)
+    except:
+        pass
 
-    if time_diff_from_prev == -1:
-        logging.info("Pulse counting: no period detected, waiting for next measurmeent period")
-        return
+    calculated_value = (edge_cnt / 2)*pcnt_multiplier
+    set_value_float(measurements, "rain_gauge_height", calculated_value, SenmlSecondaryUnits.SENML_SEC_UNIT_MILLIMETER)
+
+    logging.debug("   ")
+    logging.debug("================ {} =======================".format(label))
+    logging.debug("========= pcnt_count: {}, edge_cnt: {}, time_diff_from_prev: {}, calculated_value: {}".format(pulse_cnt, edge_cnt, time_diff_from_prev, calculated_value) )
+    logging.debug("=============================================")
 
 
-def execute(measurements, port_number, is_high_freq, formula):
+def execute(measurements, port_number, is_high_freq, formula, label=""):
     global _is_first_run
+    global _is_after_initialization
     if machine.reset_cause() != machine.DEEPSLEEP_RESET and _is_first_run:
         init_ulp(port_number, is_high_freq)
-    else:
-        init_gpio(port_number)
-    read_ulp_values(measurements, formula)
+        _is_after_initialization = True
+    read_ulp_values(measurements, formula, label)
     _is_first_run = False
