4b98b68f2818e87bb9a9812aad21df95f1daf82f
Nikolaos Ftylitakis <ftylitak@gmail.com>
2024-06-14 16:28:37 +0300

    unify pin configurations into common_config.json

--- insighioNode/apps/demo_console/templ/device_scale_config.py
+++
@@ -1,6 +0,0 @@
-meas-scale-offset = <meas-scale-offset>
-meas-scale-scale = <meas-scale-scale>
-
-meas-scale-enabled = <meas-scale-enabled>
-
-meas-scale-monitoring-enabled = <meas-scale-monitoring-enabled>
