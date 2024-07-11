cfc8a44f170ed10ffcd06d69409bf49ed96068b3
Nikolaos Ftylitakis <ftylitak@gmail.com>
2024-07-11 16:31:38 +0300

    populating unit tests for cfg module

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -55,9 +55,13 @@ def test_init_tmp_config():
     module_utils.existsFile.side_effect = [True]
 
     module_utils.readFromFile = Mock()
-    module_utils.readFromFile.side_effect = ['{"user_test_tmp":"1"}', '{"device_test_tmp":"1"}']
+    module_utils.readFromFile.side_effect = ['{"user_test_tmp":"1"}', '{"device_test_tmp":"2"}']
 
     assert cfg.init() == True
+    assert "device_test_tmp" in cfg.device_settings
+    assert "user_test_tmp" in cfg.user_settings
+    assert cfg.device_settings["device_test_tmp"] == "2"
+    assert cfg.user_settings["user_test_tmp"] == "1"
 
 
 def test_init_config():
@@ -65,6 +69,48 @@ def test_init_config():
     module_utils.existsFile.side_effect = [False, True]
 
     module_utils.readFromFile = Mock()
-    module_utils.readFromFile.side_effect = ['{"user_test_tmp":"2"}', '{"device_test_tmp":"2"}']
+    module_utils.readFromFile.side_effect = ['{"user_test_tmp":"3"}', '{"device_test_tmp":"4"}']
 
     assert cfg.init() == True
+    assert "device_test_tmp" in cfg.device_settings
+    assert "user_test_tmp" in cfg.user_settings
+    assert cfg.device_settings["device_test_tmp"] == "4"
+    assert cfg.user_settings["user_test_tmp"] == "3"
+
+
+def test_internal_has_obj_none():
+    assert cfg._has(None, "key_one") == False
+
+
+def test_internal_has_key_none():
+    testObj = {"test": 1}
+    assert cfg._has(testObj, None) == False
+
+
+def test_internal_has_not_exist():
+    testObj = {"test": 1}
+    assert cfg._has(testObj, "key_one") == False
+
+
+def test_internal_has_exist():
+    testObj = {"test": 1}
+    assert cfg._has(testObj, "test") == True
+
+
+def test_internal_get_obj_none():
+    assert cfg._get(None, "key_one") == None
+
+
+def test_internal_get_key_none():
+    testObj = {"test": 1}
+    assert cfg._get(testObj, None) == None
+
+
+def test_internal_get_not_exist():
+    testObj = {"test": 1}
+    assert cfg._get(testObj, "key_one") == None
+
+
+def test_internal_get_exist():
+    testObj = {"test": 1}
+    assert cfg._get(testObj, "test") == 1
