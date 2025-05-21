36e3ebe59860c94488e9515de6945fe2550d18c8
Nikolaos Ftylitakis <ftylitak@gmail.com>
2025-05-21 13:03:08 +0300

    cleanup non-usable comments

--- insighioNode/apps/demo_console/templ/shield_enviro_templ.py
+++ insighioNode/apps/demo_console/templ/shield_enviro_templ.py
@@ -22,7 +22,6 @@ _UC_IO_MODBUS_DRV_ON = 37
 _UC_IO_ADS_ADDR = 0x48
 _ADS_RATE = 1
 
-# needs revisiting
 
 """ Pulse counter """
 UC_IO_DGTL_SNSR_1_READ = 4
@@ -31,11 +30,7 @@ UC_IO_DGTL_SNSR_2_READ = 5
 """ Chip ID """
 _I2C_CHIP_ID_ADDRESS = 0x54
 
-# prettier-ignore-start
-# eslint-disable-next-line
 _MEAS_SDI12 = '<meas-sdi12>'
 _MEAS_MODBUS = '<meas-modbus>'
 _MEAS_ADC = '<meas-adc>'
 _MEAS_PULSECOUNTER = '<meas-pulseCounter>'
-
-# prettier-ignore-end
