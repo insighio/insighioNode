1a2d2c0677257ece6e75fa0577724b8d0577a465
Nikolaos Ftylitakis <ftylitak@gmail.com>
2025-12-16 18:12:22 +0200

    updated utils to support flag files + data partition

--- insighioNode/lib/utils/__init__.py
+++ insighioNode/lib/utils/__init__.py
@@ -6,19 +6,47 @@ key_value_storage = esp32.NVS("insighio")
 
 _DATA_DIR = "/data"
 
+
+def list_files_recursive(folder):
+    files = []
+    for entry in uos.ilistdir(folder):
+        name = entry[0]
+        path = folder + "/" + name
+        if entry[1] == 0x4000:  # directory
+            files += list_files_recursive(path)
+        else:
+            files.append(path)
+    return files
+
+
 def existsFile(source):
     try:
-        print("existsFile: " + source)
+        logging.debug("existsFile: " + source)
         uos.stat(source)
         return True
     except Exception as e:
         return False
 
+
 _USE_DATA_DIR = existsFile("/data")
+
+
+def update_USE_DATA_DIR():
+    global _USE_DATA_DIR
+    _USE_DATA_DIR = existsFile("/data")
+    if not _USE_DATA_DIR:
+        logging.debug("No /data directory found. Using root directory.")
+    else:
+        logging.debug("Using /data directory.")
+
+
+def data_partition_in_use():
+    return _USE_DATA_DIR
+
 
 def copyFile(source, destination):
     try:
-        print("copyFile: " + source + ", " + destination)
+        logging.debug("copyFile: " + source + ", " + destination)
         in_file = open(source, "r")
         out_file = open(destination, "w")
         contents = in_file.read()
@@ -30,18 +58,20 @@ def copyFile(source, destination):
         logging.exception(e, "Error copying file [{}] to [{}]".format(source, destination))
         return False
 
+
 def renameFile(source, destination):
     try:
-        print("renameFile: " + source + ", " + destination)
+        logging.debug("renameFile: " + source + ", " + destination)
         uos.rename(source, destination)
         return True
     except Exception as e:
         logging.exception(e, "Error renaming file [{}] to [{}]".format(source, destination))
         return False
 
+
 def readFromFile(source):
     try:
-        print("readFromFile: "+ source)
+        logging.debug("readFromFile: " + source)
         f = open(source, "r")
         contents = f.read()
         f.close()
@@ -50,13 +80,15 @@ def readFromFile(source):
         logging.error("Error reading file [{}]".format(source))
         return ""
 
+
 def appendToFile(destination, content):
-    print("appendToFile: " + destination )
+    logging.debug("appendToFile: " + destination)
     return writeToFile(destination, content, True)
+
 
 def writeToFile(destination, content, do_append=False):
     try:
-        print("writeToFile: " + destination)
+        logging.debug("writeToFile: " + destination)
         with open(destination, "w" if not do_append else "a") as file:
             file.write(content)
         return True
@@ -64,18 +96,20 @@ def writeToFile(destination, content, do_append=False):
         logging.exception(e, "Error writing to file [{}]".format(destination))
         return False
 
+
 def deleteFile(destination):
     try:
-        print("deleteFile: " + destination)
+        logging.debug("deleteFile: " + destination)
         uos.remove(destination)
     except Exception as e:
         logging.exception(e, "Error deleting file [{}]".format(destination))
         return False
 
+
 def countFileLines(source):
     lines = 0
     try:
-        print("countFileLines: " + source)
+        logging.debug("countFileLines: " + source)
         with open(source) as f:
             lines = len(f.readlines())
     except:
@@ -83,48 +117,60 @@ def countFileLines(source):
 
     return lines
 
+
 ############ Auxilary file system functions
+
 
 def decorateFlagPath(path):
     if _USE_DATA_DIR:
         path = _DATA_DIR + ("" if path.startswith("/") else "/") + path
     return path
 
+
 def existsFlagFile(source):
     source = decorateFlagPath(source)
     return existsFile(source)
 
+
 def copyFlagFile(source, destination):
     source = decorateFlagPath(source)
-    destination =  decorateFlagPath(destination)
+    destination = decorateFlagPath(destination)
     return copyFile(source, destination)
+
 
 def renameFlagFile(source, destination):
     source = decorateFlagPath(source)
-    destination =  decorateFlagPath(destination)
+    destination = decorateFlagPath(destination)
     return copyFile(source, destination)
+
 
 def readFromFlagFile(source):
     source = decorateFlagPath(source)
     return readFromFile(source)
 
+
 def appendToFlagFile(destination, content):
     destination = decorateFlagPath(destination)
     return appendToFile(destination, content)
 
+
 def writeToFlagFile(destination, content):
-    destination =  decorateFlagPath(destination)
+    destination = decorateFlagPath(destination)
     return writeToFile(destination, content, False)
 
+
 def deleteFlagFile(destination):
-    destination =  decorateFlagPath(destination)
+    destination = decorateFlagPath(destination)
     return deleteFile(destination)
 
+
 def countFlagFileLines(destination):
-    destination =  decorateFlagPath(destination)
+    destination = decorateFlagPath(destination)
     return countFileLines(destination)
 
+
 #########################################################3
+
 
 # key max length: 15 chars
 def getKeyValueInteger(key):
@@ -154,6 +200,11 @@ def eraseKeyValue(key):
 
 def clearCachedStates():
     eraseKeyValue("tz_sec_offset")
+    ULP_TIMESTAMP_FLAG_FILE = "/pcnt_last_read_timestamp"
+    deleteFlagFile(ULP_TIMESTAMP_FLAG_FILE)
+
+
+def requestFileSystemOptimization():
     deleteFile("/perfOk")
 
 
@@ -161,12 +212,12 @@ def deleteModule(module_name):
     try:
         import gc
 
-        print(gc.mem_free())
+        logging.debug(gc.mem_free())
         import sys
 
         del sys.modules[module_name]
         del module_name
-        print(gc.mem_free())
+        logging.debug(gc.mem_free())
         gc.collect()
     except Exception as e:
         logging.exception(e, "===")
