6a7b7b7d5ccb7d725b0fa4ed20547b8a125ea1b3
Nikolaos Ftylitakis <ftylitak@gmail.com>
2026-04-02 16:03:19 +0300

    update microsdi12 lib from v0.7.2 to v0.8.0

---
+++ insighioNode/apps/demo_console/templ/shield_enviro_v2_templ.py
@@ -0,0 +1,38 @@
+_SELECTED_SHIELD = _CONST_SHIELD_ENVIRO_V2
+
+""" SDI-12 """
+
+_UC_IO_SDI_12_RX_ON = 8
+_UC_IO_SDI_12_RX = 9
+_UC_IO_SDI_12_TX_ON = 11
+_UC_IO_SDI_12_TX = 41
+_UC_IO_SDI_12_REG_ON = 42
+_UC_IO_SNSR_REG_ON = 48
+
+
+_UC_IO_EXPANDER_ADDR = 0x20
+
+""" Modbus """
+
+_UC_IO_MODBUS_DRV_IN = 1
+_UC_IO_MODBUS_RCV_ON = 2
+_UC_IO_MODBUS_RCV_OUT = 17
+_UC_IO_MODBUS_DRV_ON = 37
+
+
+""" ADS """
+_UC_IO_ADS_ADDR = 0x48
+_ADS_RATE = 1
+
+
+""" Pulse counter """
+UC_IO_DGTL_SNSR_1_READ = 4
+UC_IO_DGTL_SNSR_2_READ = 5
+
+""" Chip ID """
+_I2C_CHIP_ID_ADDRESS = 0x54
+
+_MEAS_SDI12 = "<meas-sdi12>"
+_MEAS_MODBUS = "<meas-modbus>"
+_MEAS_ADC = "<meas-adc>"
+_MEAS_PULSECOUNTER = "<meas-pulseCounter>"
